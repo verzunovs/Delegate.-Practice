a90a6d22ac0123bbf842b4d0151176b28b9d4dea
Serhii Verzunov <shvabroid1221@gmail.com>
2020-12-21 20:26:15 +0200

    Changed program class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,14 @@
 {
     public class Program
     {
-        private static ResultHandler result = Class2.Result;
+        private static ResultHandler result;
         private static ShowHandler show = Show;
 
         private static void Main(string[] args)
         {
+            PowHandler pow = null;
+            var c2 = new Class2();
+            result = c2.Calc(pow, 10, 9);
             bool flag = result(10);
             show(flag);
         }
